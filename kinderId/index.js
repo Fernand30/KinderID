8f11f1ab74fc7bc7e56c8cff89fef3dd901dace4
seegal <seegalbird@mail.com>
2018-03-20 20:24:38 -0700

    react native expo

---
+++ kinderId/index.js
@@ -0,0 +1,5 @@
+import { AppRegistry } from 'react-native';
+import App from './src/root';
+
+AppRegistry.registerComponent('KinderID', () => App);
+AppRegistry.registerComponent ( 'main', () => App);
